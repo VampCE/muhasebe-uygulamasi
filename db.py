2fff544e191b1d974b69002ee0a76097770572d7
VampCE <167545780+VampCE@users.noreply.github.com>
2026-01-18 19:28:48 +0300

    Refactor database connection retrieval logic

--- db.py
+++ db.py
@@ -5,8 +5,13 @@ import streamlit as st
 def get_conn():
     db_url = None
 
-    if "DATABASE_URL" in st.secrets:
-        db_url = st.secrets["DATABASE_URL"]
+    if "DATABASE_URL" in st.secrets
+    db_url = st.secrets.get("DATABASE_URL")
+    try:
+        return psycopg2.connect(db_url)
+    except Exception as e:
+        st.error(f"DB hata: {type(e).__name__} - {e}")
+        raise
     else:
         db_url = os.getenv("DATABASE_URL")
 
